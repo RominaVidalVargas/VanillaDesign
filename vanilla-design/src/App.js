90a5aca0ecdf9a0639ee8ee20e05b4ae93c8fc0b
RominaVidalVargas <romina.vidal@gmail.com>
2024-06-17 18:40:51 -0400

    layout and progress

--- vanilla-design/src/App.js
+++ vanilla-design/src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
+import logo from './img/logo.ico';
+import fondo from './img/fondo.png';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Vanilla Diseño
-        </p>
-      </header>
+
+<div className='container'>
+  
+  <p className='textHeader'>Texto de header top</p>
+    <nav className="navbar bg-body-tertiary">
+      <navbar> 
+      <img src={logo} height={40} width={90} alt="logo" className='logo' ></img>  
+      </navbar> 
+    <div className="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+    <button variant="link" className='button' >Inicio</button>
+    <button variant="link" className='button'>Sección 2</button>
+    <button variant="link" className='button'>Sección 3</button>
     </div>
+    </nav> 
+
+    <div className='box'>
+    <img src={fondo} fluid alt="fondo"></img>
+    <p className='text1'>Texto previo al título</p>
+    <p className='text2'>Título banner principal</p>
+    <button className='buttonSend'>Enviar</button>
+    </div>
+</div>
+
   );
 }
 
