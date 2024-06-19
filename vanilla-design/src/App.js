b3869bc2fa4d893dbc62aad3341f667376f0a318
RominaVidalVargas <romina.vidal@gmail.com>
2024-06-18 22:01:34 -0400

    progress

--- vanilla-design/src/App.js
+++ vanilla-design/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import logo from './img/logo.ico';
 import fondo from './img/fondo.png';
+import item from './img/item.png';
+
 
 
 
@@ -10,9 +12,10 @@ function App() {
 <div className='container'>
   
   <p className='textHeader'>Texto de header top</p>
+    
     <nav className="navbar bg-body-tertiary">
       <navbar> 
-      <img src={logo} height={40} width={90} alt="logo" className='logo' ></img>  
+      <img src={logo} height={40} width={100} alt="logo" className='logo' ></img>  
       </navbar> 
     <div className="btn-group" role="group" aria-label="Basic checkbox toggle button group">
     <button variant="link" className='button' >Inicio</button>
@@ -23,9 +26,35 @@ function App() {
 
     <div className='box'>
     <img src={fondo} fluid alt="fondo"></img>
-    <p className='text1'>Texto previo al título</p>
-    <p className='text2'>Título banner principal</p>
+    <h1 className='text1'>Texto previo al título</h1>
+    <h1 className='text2'>Título banner principal</h1>
     <button className='buttonSend'>Enviar</button>
+    </div>
+
+    <div className='items'>
+      <div className='card'>
+        <img src={item} alt="item" className='imgItem'></img>
+        <h1>Item 1</h1>
+        <p>Aún nuestro sitio está en construcción (somos muy perfeccionistas jeje).
+        Mientras puedes navegar en nuestro sitio antiguo.</p>
+        </div>
+
+        <div className='card'>
+        <img src={item} alt="item" className='imgItem'></img>
+        <h1>Item 1</h1>
+        <p>Aún nuestro sitio está en construcción (somos muy perfeccionistas jeje).
+        Mientras puedes navegar en nuestro sitio antiguo.</p>
+        </div>
+        
+        <div className='card'>
+        <img src={item} alt="item" className='imgItem'></img>
+        <h1>Item 1</h1>
+        <p>Aún nuestro sitio está en conntrucción (somos muy perfeccionistas jeje).
+        Mientras puedes navegar en nuestro sitio antiguo.</p>
+        </div>
+    
+
+
     </div>
 </div>
 
